141402b9fbae8c2e182dff6c9727d6e7225e57c0
motformon <motformon@gmail.com>
2019-01-05 22:15:21 +0300

    create login

--- src/express/config/keys.js
+++ src/express/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://mikhail26:mikhail26@ds145184.mlab.com:45184/crm-react-express'
+	mongoURI: 'mongodb://mikhail26:mikhail26@ds145184.mlab.com:45184/crm-react-express',
+	jwt: 'dev-jwt'
 }
