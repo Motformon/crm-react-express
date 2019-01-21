580592dd2119595859b4be6577ce612acaf587cb
motformon <motformon@gmail.com>
2019-01-21 16:41:40 +0300

    Functional positions

--- controllers/position.js
+++ controllers/position.js
@@ -16,7 +16,13 @@ module.exports.getByCategoryId = function(req, res) {
 }
 module.exports.create = function(req, res) {
 	try {
-	 	
+	 	const position = await new Position({
+			 name: req.body.name,
+			 cost: req.body.cost,
+			 category: req.body.category,
+			 user: req.user.id,
+		 }).save();
+		 res.status(201).json(position);
 	} catch(e) {
 		errorHandler(res,e);
 	}
