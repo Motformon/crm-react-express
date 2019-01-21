eb51c3cff2a9d0413b3e960c257c77e79b8e0aaa
motformon <motformon@gmail.com>
2019-01-21 17:15:57 +0300

    Getting and remove categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,25 +1,49 @@
-module.exports.getAll = function(req, res) {
-	res.json({
-		message: 'from controller getAll'
-	})
+const Category = require('../models/Category')
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = async function(req, res) {
+  try {
+    const categories = await Category.find({user: req.user.id})
+    res.status(200).json(categories)
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
-module.exports.getById = function(req, res) {
-	res.status(200).json({
-		getById: 'from controller getById'
-	})
+
+module.exports.getById = async function(req, res) {
+  try {
+    const category = await Category.findById(req.params.id)
+    res.status(200).json(category)
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
-module.exports.remove = function(req, res) {
-	res.status(200).json({
-		remove: 'from controller remove'
-	})
+
+module.exports.remove = async function(req, res) {
+  try {
+    await Category.remove({_id: req.params.id})
+    await Position.remove({category: req.params.id})
+    res.status(200).json({
+      message: 'Категория удалена.'
+    })
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
+
 module.exports.create = function(req, res) {
-	res.status(200).json({
-		create: 'from controller create'
-	})
+  try {
+
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
+
 module.exports.update = function(req, res) {
-	res.status(200).json({
-		update: 'from controller update'
-	})
+  try {
+
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
