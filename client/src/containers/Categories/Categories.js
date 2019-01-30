0aadb905ee8e69dc57ecb36b1e66afc47dc40bc0
motformon <motformon@gmail.com>
2019-01-30 18:52:11 +0300

    add container Categories

--- client/src/containers/Categories/Categories.js
+++ client/src/containers/Categories/Categories.js
@@ -1,17 +1,36 @@
 import React, {Component} from 'react'
-
+import Sidebar from '../../components/Navigation/Sidebar/Sidebar'
+import FloatingButton from '../../components/Navigation/FloatingButton/FloatingButton'
 class Categories extends Component {
-
-  state = {
-   
-  }
-
 
   render() {
     return (
-      <div>
-3
-      </div>
+      <React.Fragment>
+				<Sidebar/>
+					<main className="content">
+							<div className="page-title">
+									<h4>Категории</h4>
+									<button className="waves-effect waves-light btn grey darken-1">Добавить категорию</button>
+							</div>
+
+							<div className="row">
+									<div className="col s12">
+											<div className="collection">
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item active">Категория 2</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+													<a href="#!" className="collection-item">Категория 1</a>
+											</div>
+									</div>
+							</div>
+
+					</main>
+				<FloatingButton/>
+			</React.Fragment>
     )
   }
 }
