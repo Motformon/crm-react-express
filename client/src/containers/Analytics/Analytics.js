0e518ce627cf37f682fe45797af67256e1f43c51
motformon <motformon@gmail.com>
2019-01-30 18:43:44 +0300

    add container Analytics

--- client/src/containers/Analytics/Analytics.js
+++ client/src/containers/Analytics/Analytics.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react'
-
+import Sidebar from '../../components/Navigation/Sidebar/Sidebar'
+import FloatingButton from '../../components/Navigation/FloatingButton/FloatingButton'
 class Analytics extends Component {
-
-  state = {
-   
-  }
-
 
   render() {
     return (
-      <div>
-2
-      </div>
+      <React.Fragment>
+				<Sidebar/>
+					<main className="content">
+							<div className="page-title">
+									<h4>Аналитика</h4>
+							</div>
+
+							<div className="average-price">
+									<p>Средний чек <strong>1500 р.</strong></p>
+							</div>
+
+							<div className="analytics-block pb3">
+									<h5>Выручка</h5>
+									<canvas></canvas>
+							</div>
+
+							<div className="analytics-block">
+									<h5>Заказы</h5>
+									<canvas></canvas>
+							</div>
+					</main>
+				<FloatingButton/>
+			</React.Fragment>
     )
   }
 }
-
+ 
 
 
 export default Analytics;
