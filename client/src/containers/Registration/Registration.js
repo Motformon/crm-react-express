2b79640fff394c26cc51b17ca20fb214dec6e636
motformon <motformon@gmail.com>
2019-02-25 18:58:26 +0300

    create user on registration

--- client/src/containers/Registration/Registration.js
+++ client/src/containers/Registration/Registration.js
@@ -1,28 +1,67 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import NavStart from '../../components/Navigation/NavStart/NavStart';
 class Registration extends Component {
+
+  state = {
+		email: '',
+		password: ''
+	}
+
+	 registerHandler = async (event) => {
+		event.preventDefault();
+		try {
+			const data = {
+				email: this.state.email.toLowerCase(),
+				password: this.state.password
+			}			
+
+			const response = await axios.post('/api/auth/register/',data);
+			alert('Аккаунт создан');
+	
+			console.log(response.data);
+
+		} catch (e) {
+			console.log(e)
+			if(e.message === 'Request failed with status code 409') {
+				alert('Такой email уже существует');
+			}
+		}
+
+	}
+
+	emailValue = (event) => {
+		this.setState({email: event.target.value});
+	}
+
+	passwordValue = (event) => {
+		this.setState({password: event.target.value});
+	}
 
   render() {
     return (
       <React.Fragment>
 				<NavStart/>
 				<div className="auth-block">
-						<div className="card">
+						<form 
+							className="card"
+							onSubmit = {this.registerHandler}
+						>
 								<div className="card-content">
 										<span className="card-title">Создать аккаунт</span>
 										<div className="input-field">
-												<input id="email" type="email" required/>
-												<label for="email">Email:</label>
+												<input id="email" value={this.state.email} onChange={this.emailValue} type="email" required/>
+												<label htmlFor="email">Email:</label>
 										</div>
 										<div className="input-field">
-												<input id="password" type="password" required/>
-												<label for="password">Пароль:</label>
+												<input id="password" value={this.state.password} onChange={this.passwordValue} type="password" required/>
+												<label htmlFor="password">Пароль:</label>
 										</div>
 								</div>
 								<div className="card-action">
 										<button className="modal-action btn waves-effect">Создать</button>
 								</div>
-						</div>
+						</form>
 				</div>
       </React.Fragment>
     )
